79d3133d40126d5ff60a785417bc368c0f77d257
VBTX <jv.svedaite@gmail.com>
2019-07-17 12:43:58 -0500

    add bcrypt and has-secure_password and validations for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+	has_secure_password
+	has_many :items
+	validates :email, presence: true
+	validates :email, uniqueness: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_14_205936) do
+ActiveRecord::Schema.define(version: 2019_07_17_173604) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,14 @@ ActiveRecord::Schema.define(version: 2019_07_14_205936) do
     t.integer "price"
     t.string "img_url"
     t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
