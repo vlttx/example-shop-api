91a505edfa75d984dac1197e8784ee987c379bb0
VBTX <jv.svedaite@gmail.com>
2019-07-17 13:12:39 -0500

    add items to users and user id to items

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+victoria = User.create(email: "victoria@gmail.com", password: "password")
 
-	
-
+john = User.create(email: "johna@gmail.com", password: "password")
 
