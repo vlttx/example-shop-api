ba39c355d4ff9c82679d6d58d0881613e3bb2f2a
VBTX <jv.svedaite@gmail.com>
2019-07-17 14:58:23 -0500

    add user authentication during session creation

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,14 @@
 class SessionsController < ApplicationController
 	def create
-		@user = User.find(email: params[:email])
-		current_user = User.create
+		@user = User.find_by(email: params[:user][:email])
+
+		if @user && @user.authenticate(params[:user][:password])
+			render json: @user
+		else 
+			render json: {
+				error: "Invalid credentials"
+			}, status: :unauthorized
+		end
 	end
 
 
